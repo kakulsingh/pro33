2ece879db65d3ebf3359b9810dd84328f357021a
kakulsingh <73896965+kakulsingh@users.noreply.github.com>
2021-05-19 13:06:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg,ground,groundImg;
+
+var ice=[];
+var maxSnow=100;
+
+function preload(){
+  bg=loadImage("snow2.jpg");
+  groundImg=loadImage("ground.PNG");
+}
+
+function setup() {
+  createCanvas(1300,600);
+  
+  engine=Engine.create();
+  world= engine.world;
+  
+
+
+ground=createSprite(650,670);
+ground.addImage(groundImg);
+ground.scale=3.2;
+ground.velocityX=-10;
+
+
+
+if(frameCount % 275 === 0){
+  for(var i=0; i<maxSnow; i++){
+  ice.push(new Snow(random(0,1350), random(0,50)));
+  }
+  }
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+
+  
+
+  if(ground.x < 530){
+    ground.x=600;
+  }
+
+  
+//add gravity
+
+  for(var i = 0;i < maxSnow; i++){
+    ice[i].display();
+    ice[i].changePosition();
+    }    
+    
+
+
+ground.display();
+
+  
+  drawSprites();
+
+}
